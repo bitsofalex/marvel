974d156bd6e1a2b7907fb84be2dfea5138d8a5c8
Looia <alex.looi@auspost.com.au>
2017-06-20 15:05:02 +1000

    chore: remove unused

--- src/MarvelApp.tsx
+++ src/MarvelApp.tsx
@@ -1,17 +1,13 @@
 import * as React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {NavBar} from './components';
-import './App.css';
 
 class MarvelApp extends React.Component<{}, null> {
   render() {
     return (
       <MuiThemeProvider>
-        <div className="App">
+        <div>
           <NavBar />
-          <p className="App-intro">
-            To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
         </div>
       </MuiThemeProvider>
     );
