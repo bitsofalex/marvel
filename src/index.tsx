79b4064cb12e929472dbcc87eea4fce86272f9c6
Looia <alex.looi@auspost.com.au>
2017-06-20 14:29:21 +1000

    chore: add Roboto font and move css to styles folder

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import MarvelApp from './MarvelApp';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import './styles/index.css';
 import * as injectTapEventPlugin from 'react-tap-event-plugin';
 
 injectTapEventPlugin();
