2dbd0316fbd9304a7fdbdc879b6df7e516688217
Looia <alex.looi@auspost.com.au>
2017-06-27 23:31:40 +1000

    feat: add cards list to app component and use react router to show comics for root url

--- src/components/MarvelApp/MarvelApp.tsx
+++ src/components/MarvelApp/MarvelApp.tsx
@@ -1,15 +1,36 @@
 import * as React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import {NavBar} from '../NavBar';
+import * as config from '../../config/config';
+import { NavBar, Footer, ComicCardList } from '../../components';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { inject, observer } from 'mobx-react';
+import { ComicStore } from '../../stores/ComicStore';
+import { style } from 'typestyle';
 
-export class MarvelApp extends React.Component<{}, {}> {
+interface MarvelAppProps {
+  comicStore?: ComicStore;
+}
+
+const appClass = style({ display: 'flex', flexDirection: 'column', minHeight: '100vh' });
+
+@inject('comicStore')
+@observer
+export class MarvelApp extends React.Component<MarvelAppProps, {}> {
+
   render() {
     return (
-      <MuiThemeProvider>
-        <div>
-          <NavBar />
-        </div>
+      <MuiThemeProvider muiTheme={config.customTheme}>
+        <Router>
+          <div className={appClass}>
+            <NavBar />
+            <Switch>
+              <Route path="/" render={props => <ComicCardList store={this.props.comicStore!} {...props} />} />
+            </Switch>
+            <Footer />
+          </div>
+        </Router>
       </MuiThemeProvider>
     );
   }
+
 }
