f0dc3a83251232a2596400bf4b0a3f86d1dd8418
Looia <alex.looi@auspost.com.au>
2017-06-27 23:21:38 +1000

    feat: inject mobx store into app

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {MarvelApp} from './components';
+import { MarvelApp } from './components';
 import registerServiceWorker from './registerServiceWorker';
 import './styles/index.css';
 import * as injectTapEventPlugin from 'react-tap-event-plugin';
+import { Provider } from 'mobx-react';
+import { ComicStore } from './stores/ComicStore';
 
 injectTapEventPlugin();
 
+const stores = { comicStore: new ComicStore() };
+
 ReactDOM.render(
-  <MarvelApp />,
+  <Provider {...stores}>
+    <MarvelApp />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
