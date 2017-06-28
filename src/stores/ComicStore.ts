c1419608b096789ca1efcdee86be4f1ce70d60db
Looia <alex.looi@auspost.com.au>
2017-06-28 16:04:57 +1000

    fix: clears the comics before loading new offset

--- src/stores/ComicStore.ts
+++ src/stores/ComicStore.ts
@@ -1,4 +1,4 @@
-import { observable, action } from 'mobx';
+import { observable, action, computed, IObservableArray } from 'mobx';
 import { Comic } from '../apis/marvel/marvelApis';
 import * as marvelApis from '../apis/marvel/marvelApis';
 import { config } from '../config/config';
@@ -6,17 +6,22 @@ import { config } from '../config/config';
 export class ComicStore {
 
   @observable isLoading: boolean = false;
-  @observable comics: Comic[] = observable([]);
+  @observable comics: IObservableArray<Comic> = observable([]);
   @observable offset: number;
   @observable limit: number;
   @observable total: number;
   @observable count: number;
+
+  @computed get pageCount() {
+    return this.total / this.limit;
+  }
 
   @action loadComics(offset: number): Promise<void> {
     this.isLoading = true;
     return marvelApis.getComics(config.countPerPage, offset)
       .then(result => {
         this.isLoading = false;
+        this.comics.clear();
         result.data.results.forEach((comic: Comic) => this.comics.push(comic));
         this.offset = result.data.offset;
         this.limit = result.data.limit;
